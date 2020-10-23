9011ea8d88b79fe430f1578c114e5218ccff94a6
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2020-10-23 18:12:29 +0200

    wip

--- Classes/Backend/BackendModule.php
+++ Classes/Backend/BackendModule.php
@@ -33,18 +33,12 @@ use AOE\Crawler\Configuration\ExtensionConfigurationProvider;
 use AOE\Crawler\Controller\CrawlerController;
 use AOE\Crawler\Converter\JsonCompatibilityConverter;
 use AOE\Crawler\Domain\Repository\QueueRepository;
-use AOE\Crawler\Hooks\CrawlerHookInterface;
 use AOE\Crawler\Service\ProcessService;
 use AOE\Crawler\Value\CrawlAction;
 use AOE\Crawler\Value\ModuleSettings;
-use Psr\Http\Message\UriInterface;
-use TYPO3\CMS\Backend\Routing\UriBuilder;
-use TYPO3\CMS\Backend\Template\ModuleTemplate;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\QueryBuilder;
-use TYPO3\CMS\Core\Http\Uri;
-use TYPO3\CMS\Core\Imaging\Icon;
 use TYPO3\CMS\Core\Imaging\IconFactory;
 use TYPO3\CMS\Core\Localization\LanguageService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -201,6 +195,7 @@ class BackendModule
         $this->id = (int) GeneralUtility::_GP('id');
         // Setting MOD_MENU items as we need them for logging:
         $this->pObj->MOD_MENU = array_merge($this->pObj->MOD_MENU, $this->getModuleMenu());
+        $this->moduleSettings = ModuleSettings::fromArray($this->pObj->MOD_SETTINGS);
     }
 
     /**
@@ -231,119 +226,10 @@ class BackendModule
             $this->pObj->MOD_MENU['crawlaction']
         );
 
-        $theOutput = '<h2>' . htmlspecialchars($this->getLanguageService()->getLL('title')) . '</h2>' . $actionDropdown;
+        $theOutput = '<h2>' . htmlspecialchars($this->getLanguageService()->getLL('title'), ENT_QUOTES | ENT_HTML5) . '</h2>' . $actionDropdown;
         $theOutput .= $this->renderForm($selectedAction);
 
         return $theOutput;
-    }
-
-    /**
-     * Generates the configuration selectors for compiling URLs:
-     */
-    protected function generateConfigurationSelectors(): array
-    {
-        $selectors = [];
-        $selectors['depth'] = $this->selectorBox(
-            [
-                0 => $this->getLanguageService()->sL('LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:labels.depth_0'),
-                1 => $this->getLanguageService()->sL('LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:labels.depth_1'),
-                2 => $this->getLanguageService()->sL('LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:labels.depth_2'),
-                3 => $this->getLanguageService()->sL('LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:labels.depth_3'),
-                4 => $this->getLanguageService()->sL('LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:labels.depth_4'),
-                99 => $this->getLanguageService()->sL('LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:labels.depth_infi'),
-            ],
-            'SET[depth]',
-            $this->pObj->MOD_SETTINGS['depth'],
-            false
-        );
-
-        // Configurations
-        $availableConfigurations = $this->crawlerController->getConfigurationsForBranch((int) $this->id, (int) $this->pObj->MOD_SETTINGS['depth'] ?: 0);
-        $selectors['configurations'] = $this->selectorBox(
-            empty($availableConfigurations) ? [] : array_combine($availableConfigurations, $availableConfigurations),
-            'configurationSelection',
-            $this->incomingConfigurationSelection,
-            true
-        );
-
-        // Scheduled time:
-        $selectors['scheduled'] = $this->selectorBox(
-            [
-                'now' => $this->getLanguageService()->sL('LLL:EXT:crawler/Resources/Private/Language/locallang.xlf:labels.time.now'),
-                'midnight' => $this->getLanguageService()->sL('LLL:EXT:crawler/Resources/Private/Language/locallang.xlf:labels.time.midnight'),
-                '04:00' => $this->getLanguageService()->sL('LLL:EXT:crawler/Resources/Private/Language/locallang.xlf:labels.time.4am'),
-            ],
-            'tstamp',
-            GeneralUtility::_POST('tstamp'),
-            false
-        );
-
-        return $selectors;
-    }
-
-    /**
-     * Find Fe vars
-     */
-    protected function getResFeVars(array $resultData): array
-    {
-        if (empty($resultData)) {
-            return [];
-        }
-        $requestResult = $this->jsonCompatibilityConverter->convert($resultData['content']);
-        return $requestResult['vars'] ?? [];
-    }
-
-    /**
-     * Returns a link for the panel to enable or disable the crawler
-     *
-     * @throws \TYPO3\CMS\Backend\Routing\Exception\RouteNotFoundException
-     */
-    protected function getEnableDisableLink(bool $isCrawlerEnabled): string
-    {
-        if ($isCrawlerEnabled) {
-            return $this->getLinkButton(
-                'tx-crawler-stop',
-                $this->getLanguageService()->sL('LLL:EXT:crawler/Resources/Private/Language/locallang.xlf:labels.disablecrawling'),
-                $this->getInfoModuleUrl(['action' => 'stopCrawling'])
-            );
-        }
-        return $this->getLinkButton(
-            'tx-crawler-start',
-            $this->getLanguageService()->sL('LLL:EXT:crawler/Resources/Private/Language/locallang.xlf:labels.enablecrawling'),
-            $this->getInfoModuleUrl(['action' => 'resumeCrawling'])
-        );
-    }
-
-    /**
-     * @throws \TYPO3\CMS\Backend\Routing\Exception\RouteNotFoundException
-     */
-    protected function getModeLink(string $mode): string
-    {
-        if ($mode === 'detail') {
-            return $this->getLinkButton(
-                'actions-document-view',
-                $this->getLanguageService()->sL('LLL:EXT:crawler/Resources/Private/Language/locallang.xlf:labels.show.running'),
-                $this->getInfoModuleUrl(['SET[\'processListMode\']' => 'simple'])
-            );
-        } elseif ($mode === 'simple') {
-            return $this->getLinkButton(
-                'actions-document-view',
-                $this->getLanguageService()->sL('LLL:EXT:crawler/Resources/Private/Language/locallang.xlf:labels.show.all'),
-                $this->getInfoModuleUrl(['SET[\'processListMode\']' => 'detail'])
-            );
-        }
-        return '';
-    }
-
-    /**
-     * Returns the singleton instance of the crawler.
-     */
-    protected function findCrawler(): CrawlerController
-    {
-        if (! $this->crawlerController instanceof CrawlerController) {
-            $this->crawlerController = GeneralUtility::makeInstance(CrawlerController::class);
-        }
-        return $this->crawlerController;
     }
 
     /*****************************
@@ -352,35 +238,7 @@ class BackendModule
      *
      *****************************/
 
-    /**
-     * Create selector box
-     *
-     * @param array $optArray Options key(value) => label pairs
-     * @param string $name Selector box name
-     * @param string|array $value Selector box value (array for multiple...)
-     * @param boolean $multiple If set, will draw multiple box.
-     *
-     * @return string HTML select element
-     */
-    protected function selectorBox($optArray, $name, $value, bool $multiple): string
-    {
-        if (! is_string($value) || ! is_array($value)) {
-            $value = '';
-        }
-
-        $options = [];
-        foreach ($optArray as $key => $val) {
-            $selected = (! $multiple && ! strcmp($value, (string) $key)) || ($multiple && in_array($key, (array) $value, true));
-            $options[] = '
-                <option value="' . $key . '" ' . ($selected ? ' selected="selected"' : '') . '>' . htmlspecialchars($val) . '</option>';
-        }
-
-        return '<select class="form-control" name="' . htmlspecialchars($name . ($multiple ? '[]' : '')) . '"' . ($multiple ? ' multiple' : '') . '>' . implode('', $options) . '</select>';
-    }
-
-
-
-    protected function initializeView(): void
+    private function initializeView(): void
     {
         $view = GeneralUtility::makeInstance(StandaloneView::class);
         $view->setLayoutRootPaths(['EXT:crawler/Resources/Private/Layouts']);
@@ -390,37 +248,9 @@ class BackendModule
         $this->view = $view;
     }
 
-    protected function getLanguageService(): LanguageService
+    private function getLanguageService(): LanguageService
     {
         return $GLOBALS['LANG'];
-    }
-
-    protected function getLinkButton(string $iconIdentifier, string $title, UriInterface $href): string
-    {
-        $moduleTemplate = GeneralUtility::makeInstance(ModuleTemplate::class);
-        $buttonBar = $moduleTemplate->getDocHeaderComponent()->getButtonBar();
-        return (string) $buttonBar->makeLinkButton()
-            ->setHref((string) $href)
-            ->setIcon($this->iconFactory->getIcon($iconIdentifier, Icon::SIZE_SMALL))
-            ->setTitle($title)
-            ->setShowLabelText(true);
-    }
-
-    /**
-     * Returns the URL to the current module, including $_GET['id'].
-     *
-     * @param array $uriParameters optional parameters to add to the URL
-     *
-     * @throws \TYPO3\CMS\Backend\Routing\Exception\RouteNotFoundException
-     */
-    protected function getInfoModuleUrl(array $uriParameters = []): Uri
-    {
-        if (GeneralUtility::_GP('id')) {
-            $uriParameters['id'] = GeneralUtility::_GP('id');
-        }
-        /** @var UriBuilder $uriBuilder */
-        $uriBuilder = GeneralUtility::makeInstance(UriBuilder::class);
-        return $uriBuilder->buildUriFromRoute('web_info', $uriParameters);
     }
 
     private function getModuleMenu(): array
@@ -458,7 +288,7 @@ class BackendModule
 
     private function renderForm(CrawlAction $selectedAction): string
     {
-        $requestForm = RequestFormFactory::create($selectedAction, $this->view);
+        $requestForm = RequestFormFactory::create($selectedAction, $this->view, $this->moduleSettings, $this->pObj);
         return $requestForm->render(
             $this->id,
             $this->pObj->MOD_SETTINGS['depth'],
